22e4cd9b3edc79fad4a206a0fd62af15f8ddb724
josecosta-on <35690195+josecosta-on@users.noreply.github.com>
2023-02-13 08:56:32 +0000

    add first-setup.sh

---
+++ first-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if ! [ -f /code/.docker/setup.lock ]
+then
+echo "
+-----------------------------------------------------------------------------
+First time setup 
+-----------------------------------------------------------------------------"
+
+mkdir -p /code/.docker
+usermod -a -G root nobody
+rm -rf /code/nvm \
+    && mv $NVM_DIR /code/ \
+    && rm -rf $NVM_DIR \
+    && ln -s /code/nvm $NVM_DIR \
+    && echo $NVM_DIR \
+    && touch /code/.docker/setup.lock
+
+else
+rm -rf $NVM_DIR && ln -s /code/nvm $NVM_DIR 
+
+echo "
+-----------------------------------------------------------------------------
+No need, Already Setup
+-----------------------------------------------------------------------------"
+exit 1
+fi
