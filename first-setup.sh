b3cdddaefe51bbd83a13f80a686e5d87907bdd49
josecosta-on <35690195+josecosta-on@users.noreply.github.com>
2023-02-27 12:25:15 +0000

    Update first-setup.sh

--- first-setup.sh
+++ first-setup.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
-if ! [ -f /code/.docker/setup.lock ]
+if ! [ -f /code/.system-v1/.docker/setup.lock ]
 then
 echo "
 -----------------------------------------------------------------------------
 First time setup 
 -----------------------------------------------------------------------------"
 
-mkdir -p /code/.docker
+mkdir -p /code/.system-v1/.docker
 usermod -a -G root nobody
-rm -rf /code/nvm \
-    && mv $NVM_DIR /code/ \
+rm -rf /code/.system-v1/nvm \
+    && mv $NVM_DIR /code/.system-v1/ \
     && rm -rf $NVM_DIR \
-    && ln -s /code/nvm $NVM_DIR \
+    && ln -s /code/.system-v1/nvm $NVM_DIR \
     && echo $NVM_DIR \
-    && touch /code/.docker/setup.lock
+    && touch /code/.system-v1/.docker/setup.lock
 
 else
-rm -rf $NVM_DIR && ln -s /code/nvm $NVM_DIR 
+rm -rf $NVM_DIR && ln -s /code/.system-v1/nvm $NVM_DIR 
 exit 1
 fi
