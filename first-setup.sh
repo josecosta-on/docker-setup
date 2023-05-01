e666932bd237d17bea69dbffaf27780b0f6f561a
josecosta-on <35690195+josecosta-on@users.noreply.github.com>
2023-05-01 14:41:19 +0100

    Update first-setup.sh

--- first-setup.sh
+++ first-setup.sh
@@ -11,3 +11,5 @@ rm -rf /code/${DOCKER_CODE} \
 && ln -s /code/${DOCKER_CODE} /usr/local/${DOCKER_CODE}
 echo /code/$DIR
 touch ${DOCKER_PATH}/.first_setup-completed
+touch ${DOCKER_PATH}/.javaversion
+echo '#!/bin/bash\nsource /usr/bin/java-switch ${JAVA_CODE}\n' > ${DOCKER_PATH}/.javaversion
