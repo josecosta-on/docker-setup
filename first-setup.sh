0eb23b39ccfb625822c648369c785eed30d7c435
josecosta-on <35690195+josecosta-on@users.noreply.github.com>
2024-03-13 11:03:12 +0000

    Update first-setup.sh

--- first-setup.sh
+++ first-setup.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
+
 if test -f ${DOCKER_PATH}/.first_setup-completed; then
   exit
 fi
+
+mkdir -p ${DOCKER_PATH}
+touch ${DOCKER_PATH}/.first_setup-completed
 
 echo "
 -----------------------------------------------------------------------------
 First time setup 
 -----------------------------------------------------------------------------"
-
 usermod -a -G root nobody
-mkdir -p ${DOCKER_PATH}
 rm -rf /code/${DOCKER_CODE} \
 && mv /usr/local/${DOCKER_CODE} /code/${DOCKER_CODE} \
 && rm -rf /usr/local/${DOCKER_CODE} \
 && ln -s /code/${DOCKER_CODE} /usr/local/${DOCKER_CODE}
-echo /code/$DIR
-touch ${DOCKER_PATH}/.first_setup-completed
+
 touch ${DOCKER_PATH}/.javaversion
 chmod 777 ${DOCKER_PATH}
 echo -e '#!/bin/bash\nsource /usr/bin/java-switch ${JAVA_CODE}\n' > ${DOCKER_PATH}/.javaversion
